5dbf826bbd21db797d59596ebce5b2a76e9b3a79
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2024-05-15 16:30:36 +0000

    feat: Add Cache class to store data in Redis
    The code changes include adding a new class called Cache, which allows storing data in Redis using a random key. This class initializes an instance of the Redis client and flushes the instance using flushdb. The store method stores the input data in Redis and returns the key. This addition enhances the functionality of the code by providing a convenient way to cache and retrieve data using Redis.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,18 @@ from typing import Union
 
 
 class Cache:
+    """
+    A class for storing and retrieving data from Redis using UUIDs as keys.
+
+    Attributes:
+        self._redis (redis.Redis): A Redis client instance.
+
+    Methods:
+        __init__(): Initialize a Redis client instance and clear the database.
+        store(data): Store the data in Redis with a UUID key and
+        return the key.
+    """
+
     def __init__(self):
         """A method to store an instance of the Redis client as a private
         variable named _redis (using redis.Redis()) and flush the
